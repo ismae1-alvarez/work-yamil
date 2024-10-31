7c74f37dfa9955c785f3b1397abd14e19cfc4c95
Ismael Alvarez <ismaelalvarez514@gmail.com>
2024-10-31 17:26:23 -0600

    Integrate Statistics

--- src/components/InfoCountry.tsx
+++ src/components/InfoCountry.tsx
@@ -1,35 +1,47 @@
 import React from "react";
-import { Info } from "@/types/infoCountry"; 
-
+import { Info } from "@/types/infoCountry";
+import { LineChart } from "@mui/x-charts";
 interface CountryCardProps {
-    info: Info; 
+  info: Info;
+  years : number[];
+  population: number[]
 }
 
-const InfoCard: React.FC<CountryCardProps> = ({ info }) => {
-    return (
-        <div className="max-w rounded overflow-scroll shadow-lg ">
-            <img className="w-full h-48 object-cover" src={info.flagUrl} alt={`${info.country} flag`} />
-            <div className="p-4">
-                <h2 className="text-xl font-bold mb-2">{info.country}</h2>
-                <h3 className="text-lg font-semibold mb-2">Fronteras:</h3>
-                <ul className="list-disc list-inside mb-2">
-                    {info.borders.map((border, index) => (
-                        <li key={index}>
-                            {border.commonName} ({border.officialName})
-                        </li>
-                    ))}
-                </ul>
-                <h3 className="text-lg font-semibold mb-2">Datos de Población:</h3>
-                <ul className="list-disc list-inside mb-2">
-                    {info.populationData.map((data, index) => (
-                        <li key={index}>
-                            {data.year}: {data.value} personas
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </div>
-    );
+const InfoCard: React.FC<CountryCardProps> = ({ info,years, population }) => {
+
+    console.log(population)
+  return (
+    <div className="max-w rounded overflow-scroll shadow-lg ">
+      <img
+        className="w-full h-min object-cover justify-center"
+        src={info.flagUrl}
+        alt={`${info.country} flag`}
+      />
+      <div className="p-4">
+        <h2 className="text-xl font-bold mb-2">{info.country}</h2>
+        <h3 className="text-lg font-semibold mb-2">Fronteras:</h3>
+        <ul className="list-disc list-inside mb-2">
+          {info.borders.map((border, index) => (
+            <li key={index}>
+              {border.commonName} ({border.officialName})
+            </li>
+          ))}
+        </ul>
+
+        <h3 className="text-lg font-semibold mb-2">Datos de Población:</h3>
+        <LineChart
+          className="bg-white min-w-min p-5"
+          xAxis={[{ data: years }]}
+          series={[
+            {
+              data: population,
+            },
+          ]}
+          height={300}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default InfoCard;
--- src/pages/CountryCode.tsx
+++ src/pages/CountryCode.tsx
@@ -1,12 +1,11 @@
 import { countryCode } from "@/api/countryCode";
-import InfoCard from "@/components/InfoCountry";
 import type { Info } from "@/types/infoCountry";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import InfoCard from "@/components/InfoCountry";
 
 export default function CountryCode() {
-  const { code } = useParams();
-
+  const { code } = useParams<{ code: string }>();
   const [data, setData] = useState<Info>();
   const [loading, setLoading] = useState<boolean>(true);
 
@@ -22,15 +21,26 @@ export default function CountryCode() {
         setLoading(false);
       }
     }
-    searchCountry();
+
+    if (code) {
+      searchCountry();
+    }
   }, [code]);
 
   if (loading) {
     return <p>Cargando Datos...</p>;
   }
 
-  if (data)
+  if (data) {
+    const years = data.populationData.map((item) => item.year);
+    const population = data.populationData.map((item) => item.value);
+
     return (
-        <InfoCard key={data.country} info={data}/>
+      <>
+        <InfoCard info={data} years={years} population={population} />
+      </>
     );
+  }
+
+  return <p>No se encontraron datos para el código proporcionado.</p>;
 }
